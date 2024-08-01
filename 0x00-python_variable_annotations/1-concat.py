2be88f9c51474a1e3749755457c9be5a09a24863
Getaneh48 <getaneh11@gmail.com>
2024-08-01 15:48:14 +0000

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function concat that takes a string str1 and
+a string str2 as arguments and returns a concatenated string
+"""
+
+def concat(str1: str, str2: str) -> str:
+    '''A function that concatenates 2 strings'''
+    return str1 + str2
+
+
+if __name__ == '__main__':
+
+    str1 = 'str1'
+    str2 = 'str2'
+
+    print(concat(str1, str2) == "{}{}".format(str1, str2))
+    print(concat.__annotations__)
