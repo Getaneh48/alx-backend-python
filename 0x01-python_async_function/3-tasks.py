0fa5949d7c8db0b6ddf00f45edbbb8fd71db7997
Getaneh48 <getaneh11@gmail.com>
2024-08-05 11:29:32 +0000

    task 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import asyncio
+"""
+a module that defines a function to return a task
+"""
+
+
+def task_wait_random(n: int) -> asyncio.Task:
+    """
+    a function task_wait_random that takes an integer max_delay
+    and returns a asyncio.Task
+    """
+
+    wait_random = __import__('0-basic_async_syntax').wait_random
+    task = asyncio.create_task(wait_random(n))
+
+    return task
